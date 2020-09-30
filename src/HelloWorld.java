30c497054ce98c55ad60ef53851e4283a250370b
chengp <1660250177@qq.com>
2020-09-30 14:08:47 +0800

    联系修改

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("my first project");
+        /**
+         * 这是文档注释
+         * @author chengp
+         * @version v1.0
+         * return 0000-成功
+         */
+        String love="test1";
+        System.out.println("第一次输出："+love);
+        love="test_2";
+        System.out.println("第二次输出："+love);
     }
 }
